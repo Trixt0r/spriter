777b2ff5eb4d304d640f875cc7dc84ca413c80fc
tavo <taannu@ttu.ee>
2019-09-04 17:20:22 +0300

    make spriter framerate independent

--- src/main/java/com/brashmonkey/spriter/JsonReader.java
+++ src/main/java/com/brashmonkey/spriter/JsonReader.java
@@ -1,20 +1,23 @@
 package com.brashmonkey.spriter;
 
-import org.json.*;
+import org.json.JSONObject;
 
 import java.io.*;
 
-/** Lightweight JSON parser. Uses org.json library.
- * @author mrdlink */
+/**
+ * Lightweight JSON parser. Uses org.json library.
+ *
+ * @author mrdlink
+ */
 public class JsonReader {
 
-    private JsonReader(){}
+    private JsonReader() {}
 
-    public static JSONObject parse (String json) {
+    public static JSONObject parse(String json) {
         return new JSONObject(json);
     }
 
-    public static JSONObject parse (InputStream input) throws IOException{
+    public static JSONObject parse(InputStream input) throws IOException {
         StringBuilder textBuilder = new StringBuilder();
         Reader reader = new BufferedReader(new InputStreamReader(input));
         int c = 0;
